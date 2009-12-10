c23501a6d4d6b2282b269c4f68bc2cc3e7181d33
Robert Blanker <robert@madcap.nl>
2009-12-10 16:24:28 +0100

    Added more REST calls mediafile / asset / collections. Rewrite of the menu_hook. Added support for batch_id search. Rewrote code so its no longer requires the mediamosa response object, will now leave that to REST call object.

--- sites/all/modules/mediamosa/core/asset/not_moved.php
+++ sites/all/modules/mediamosa/core/asset/not_moved.php
@@ -74,16 +74,78 @@ function media_management_check_existence($a_args) {
 }
 
 
+
+
 /**
- * Controleer of er lopende jobs zijn voor een asset.
- * Als alle jobs gestopt zijn / of nog niet begonnen, verwijder die jobs dan.
- *
- * Code is verbeterd en verplaatst naar vpx_jobs.
- *
+ * Delete watermark image
+ * Not implemented yet!
  */
-function _media_management_delete_jobs($asset_id) {
+function media_management_delete_watermark() {
+  return new rest_response(vpx_return_error(ERRORCODE_OKAY));
 
-  return _vpx_jobs_cancel_job_by_asset($asset_id);
+  $a_parameters = array(
+    'watermark_id' => array(
+      'value' => vpx_get_parameter_2($a_args['uri'], 'watermark_id'),
+      'type' => 'alphanum',
+      'required' => TRUE,
+    ),
+    'app_id' => array(
+      'value' => vpx_get_parameter_2($a_args['get'], 'app_id'),
+      'type' => 'int',
+      'required' => TRUE,
+    ),
+    'user_id' => array(
+      'value' => vpx_get_parameter_2($a_args['get'], 'user_id'),
+      'type' => TYPE_USER_ID,
+      'required' => TRUE,
+    ),
+  );
+
+  // Validate all of the parameters
+  $result = vpx_validate($a_parameters);
+  if (vpx_check_result_for_error($result)) {
+    return new rest_response($result);
+  }
+
+  // Check whether is the webservice on
+  if (!vpx_shared_webservice_is_active('media_management', $a_parameters['app_id']['value'])) {
+    return new rest_response(vpx_return_error(ERRORCODE_WEBSERVICE_DISABLED));
+  }
+
+  $watermark_id = $a_parameters['watermark_id']['value'];
+
+// kijk of de asset bestaat en of het geen sub asset is
+  if (!vpx_count_rows("asset", array(
+    "asset_id", $asset_id,
+    "parent_id", NULL
+  ))) {
+    return new rest_response(vpx_return_error(ERRORCODE_ASSET_NOT_FOUND, array("@asset_id" => $asset_id)));
+  }
+// controleer of de still bestaat
+  if (!vpx_count_rows("mediafile", array("asset_id_root", $asset_id))) {
+    return new rest_response(vpx_return_error(ERRORCODE_STILL_NOT_FOUND, array("@asset_id" => $asset_id)));
+  }
+
+  // controleer of de gebruiker rechten heeft om de still te verwijderen
+  // get owner info
+  db_set_active('data');
+  $asset_app_id = db_result(db_query("SELECT app_id FROM {asset} where asset_id  = '%s' ", $asset_id));
+  $asset_owner  = db_result(db_query("SELECT owner_id FROM {asset} where asset_id  = '%s' ", $asset_id));
+  db_set_active();
+
+  // controleer of de gebruiker rechten heeft om de metadata aan te passen
+  try {
+    vpx_acl_owner_check($a_parameters['app_id']['value'], $a_parameters['user_id']['value'], $asset_app_id, $asset_owner);
+  }
+  catch (vpx_exception_error_access_denied $e) {
+    return $e->vpx_exception_rest_response();
+  }
+
+  // verwijder de still
+  if (($error = _media_management_delete_still($asset_id, $mediafile_id, $still_id)) === TRUE) {
+    return new rest_response(vpx_return_error(ERRORCODE_OKAY));
+  }
+  else {
+    return $error;
+  }
 }
-
-
